9c369ef9481b930dc9e924d3de78bc8cf804d9c2
PhamLeBaDuong <108616528+PhamLeBaDuong@users.noreply.github.com>
2024-06-23 19:31:43 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\PC\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\TherapistBot\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\PC\\dev\\flutter"
+  "PROJECT_DIR=E:\\TherapistBot\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\PC\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\TherapistBot\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\TherapistBot\\flutter_application_1"
+  "FLUTTER_TARGET=E:\\TherapistBot\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\TherapistBot\\flutter_application_1\\.dart_tool\\package_config.json"
+)
